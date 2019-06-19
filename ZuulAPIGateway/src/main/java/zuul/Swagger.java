9d92449cb4b7ea289a1e7fe0d443476542cc9b3f
stefanorcic <orcic.stefan@gmail.com>
2019-06-19 10:05:47 +0200

    Update Swagger.java
    swager ispravka put i delete

--- ZuulAPIGateway/src/main/java/zuul/Swagger.java
+++ ZuulAPIGateway/src/main/java/zuul/Swagger.java
@@ -20,8 +20,8 @@ public class Swagger implements SwaggerResourcesProvider {
 		List<SwaggerResource> resources = new ArrayList<>();
 		resources.add(swaggerResource("microservice-get", "/microservice-get/v2/api-docs", "2.9.2"));
 		resources.add(swaggerResource("microservice-post", "/microservice-post/v2/api-docs", "2.9.2"));
-		resources.add(swaggerResource("microservice-put", "/microservice-post/v2/api-docs", "2.9.2"));
-		resources.add(swaggerResource("microservice-delete", "/microservice-post/v2/api-docs", "2.9.2"));
+		resources.add(swaggerResource("microservice-put", "/microservice-put/v2/api-docs", "2.9.2"));
+		resources.add(swaggerResource("microservice-delete", "/microservice-delete/v2/api-docs", "2.9.2"));
 		return resources;
 	}
 
